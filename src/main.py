06dacc278209300180277cb42b53f85cdea3dfaa
Shelly Developer <shellydeveloper@Shellys-MBP.attlocal.net>
2022-11-14 17:05:20 -0500

    updated frontend view

--- src/main.py
+++ src/main.py
@@ -5,9 +5,12 @@ import numpy as np
 from fastapi import FastAPI, UploadFile,File, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import Response, FileResponse, HTMLResponse
+from fastapi.staticfiles import StaticFiles
+
+
 from PIL import Image
 
-from process import Mitosisdetection
+from src.process import Mitosisdetection
 import cv2
 import io
 import torch
@@ -16,12 +19,14 @@ from torch import autocast
 
 # instantiate the app
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="src/static"), name="static")
+
 
 #HTML Template Directory
-templates = Jinja2Templates(directory="templates") 
+templates = Jinja2Templates(directory="src/templates") 
 
 # instantiate the Mitosisdetection class
-md = Mitosisdetection()
+##md = Mitosisdetection()
 
 # create a route
 @app.get('/')
@@ -47,27 +52,31 @@ async def predict_image(request: Request, selectFile: UploadFile = File(...)):
         
         #process input image through pipeline
         # uploaded image to numpy array
-        img = Image.open(selectFile.file).convert('RGB')
+        #img = Image.open(selectFile.file).convert('RGB')
         #heatmap = Image.open(selectFile.file).convert('RGB')
-        img = np.array(img)
+        #img = np.array(img)
 
         # make predictions on image
         # result dictionary
         # superimposed image = simg
-        result, simg = md.predict(img)
+        ## result, simg = md.predict(img)
 
         # format superimposed image
-        simg = cv2.cvtColor(simg, cv2.COLOR_RGB2BGR)
-        res, simg = cv2.imencode(".png", simg)
+        ## simg = cv2.cvtColor(simg, cv2.COLOR_RGB2BGR)
+        ## res, simg = cv2.imencode(".png", simg)
         #simg = simg.tobytes()   #convert to byte object
         
         #get heatmap image
-        base64_hm_img = base64.b64encode(simg.tobytes()).decode("utf-8")
+        ##base64_hm_img = base64.b64encode(simg.tobytes()).decode("utf-8")
 
-        predDict = [{"0":"text1"},{"1":"text2"},{"0":"text3"}]
-    
-        return templates.TemplateResponse('myhtml.html', context={'request': request, 'input_img': base64_encoded_img, 'heatmap_img': base64_hm_img, 'predDict': predDict })
+        predictions = ["0","1","0"]
+        explanations = ["text1","text2","text3"]
+        xcoordinates = [{400},{10},{250}]
+        ycoordinates = [{400},{10},{250}]
+        return templates.TemplateResponse('myhtml.html', context={'request': request, 'input_img': base64_encoded_img, 'heatmap_img': base64_encoded_img, 'predDict': predictions,'explainDict': explanations, 'xcoords': xcoordinates, 'ycoords': ycoordinates})
 
     # run the app
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+#terminal command: uvicorn src.main:app --reload --workers 1 --host 0.0.0.0 --port 8000
---
+++ src/static/js/myjs.js
@@ -0,0 +1,70 @@
+
+// Switch between tabs (project details, team, inference)
+function openCity(evt, tabname) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabname).style.display = "block";
+    evt.currentTarget.className += " active";
+    }
+    document.getElementById("defaultOpen").click();
+
+
+// Code to get the coordinates on mouse move over heatmap image
+var elmids = ['hm_image'];
+var x, y = 0;       // variables that will contain the coordinates
+
+// Get X and Y position of the elm (from: vishalsays.wordpress.com)
+function getXYpos(elm) {
+x = elm.offsetLeft;        // set x to elm’s offsetLeft
+y = elm.offsetTop;         // set y to elm’s offsetTop
+
+elm = elm.offsetParent;    // set elm to its offsetParent
+
+//use while loop to check if elm is null
+// if not then add current elm’s offsetLeft to x
+//offsetTop to y and set elm to its offsetParent
+while(elm != null) {
+    x = parseInt(x) + parseInt(elm.offsetLeft);
+    y = parseInt(y) + parseInt(elm.offsetTop);
+    elm = elm.offsetParent;
+}
+// returns an object with "xp" (Left), "=yp" (Top) position
+return {'xp':x, 'yp':y};
+}
+
+// Get X, Y coords, and displays Mouse coordinates
+function getCoords(e) {
+  var xy_pos = getXYpos(this);
+
+    x = e.pageX;
+    y = e.pageY;
+    x = x - xy_pos['xp'];
+    y = y - xy_pos['yp'];
+
+  // displays x and y coords in the #coords element
+  //document.getElementById('coords').innerHTML = 'X= '+ x+ ' ,Y= ' +y;
+}
+
+// register onmousemove, and onclick the each element with ID stored in elmids
+for(var i=0; i<elmids.length; i++) {
+  if(document.getElementById(elmids[i])) {
+    // calls the getCoords() function when mousemove
+    document.getElementById(elmids[i]).onmousemove = getCoords;
+
+    // execute a function when click
+    document.getElementById(elmids[i]).onclick = function() {
+      document.getElementById('regcoords').value = x+ ' , ' +y;
+    };
+  }
+}
